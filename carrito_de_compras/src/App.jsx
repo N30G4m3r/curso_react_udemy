0482f2e62e6f144367f8e1d30085905cddecb754
Jose Montilla <neogamer1211@gmail.com>
2026-08-17 10:55:54 -0400

    comit

--- carrito_de_compras/src/App.jsx
+++ carrito_de_compras/src/App.jsx
@@ -7,10 +7,25 @@ import { db } from './data/db'
 function App() {
   //State
   const [data, setData] = useState(db)
+  const [cart, setCart] = useState([])
   //Effect
   // useEffect(() => {
   //   setData(db)
   // }, [])
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((value) => value.id === item.id)
+    console.log(`existe el item con el id (${item.id})?: ${itemExists}`)
+    if(itemExists < 0) {
+      console.log(`Agregar al carrito el item con ID: ${item.id}`)
+      item.quantity = 1
+      setCart([...cart, item])
+    } else {
+      const updatedCart = [...cart]
+      console.log(`Ya existe el item con el ID: ${item.id}, que tiene una cantidad ${}`)
+      updatedCart[itemExists].quantity++
+    }
+  }
 
   return (
     <>
@@ -21,7 +36,7 @@ function App() {
 
         <div className="row mt-5">
           {data.map((value) => (
-            <Guitar key={value.id + 'a'} id={value.id} name={value.name} image={value.image} description={value.description} price={value.price} />
+            <Guitar key={value.id} guitar={value} cart={cart} addToCart={addToCart} />
           ))}
         </div>
       </main>
--- carrito_de_compras/src/components/Guitar.jsx
+++ carrito_de_compras/src/components/Guitar.jsx
@@ -1,12 +1,18 @@
 
-export default function Guitar({ id, name, image, description, price }) {
+export default function Guitar({ guitar, addToCart }) {
 
-    console.log(`Guitar ID: ${id}, name: ${name}`)
+    const { id, name, image, description, price } = guitar
+
+    // console.log(`Guitar ID: ${id}, name: ${name}`)
+
+    const handleClick = (guitar) => {
+        addToCart(guitar)
+    }
 
     return (
-        <div id={id} className="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-                <img className="img-fluid" src={`./public/img/${image}.jpg`} alt="imagen guitarra" />
+                <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
             </div>
             <div className="col-8">
                 <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
@@ -15,6 +21,7 @@ export default function Guitar({ id, name, image, description, price }) {
                 <button
                     type="button"
                     className="btn btn-dark w-100"
+                    onClick={() => handleClick(guitar)}
                 >Agregar al Carrito</button>
             </div>
         </div>
